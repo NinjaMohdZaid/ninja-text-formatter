02d4263aeaa3e88ca704267dec81ef9decbf92c9
Ninja Mohd Zaid <nmzaid376@gmail.com>
2022-11-05 18:36:31 +0530

    Added some new features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import Alert from './components/Alert';
+import About from './components/About';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
+  const [mode,setMode] = useState('light');//whether a dark mode enabled or not
+  const [alert,setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#2f345a';
+      showAlert("Dark mode has been enabled","success");
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
+    }
+  }
   return (
-   <>
-     <NavBar title="Ninja Text Formatter"/>
-     <div className="container">
-      <TextForm heading="Enter Your text to analyze below"/>
-      {/* <About/> */}
-     </div>
-   </>
+      <>
+      <Router>
+        <NavBar title="Ninja Text Formatter" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container">
+          <Routes>
+            <Route exact path="/about"  element={<About/>}>
+            </Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Your text to analyze below" mode={mode}/>}>
+            </Route>
+          </Routes>
+        </div>
+        </Router>
+      </>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,19 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
+    function getSentanceCount(text){
+      const sentances = text.replace(/\.(?!\d)/g,'.|').split("|");
+      return sentances.length;
+    }
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert('coverted to Uppercase','success');
     }
     const handleLowerClick = (event)=>{
       let newText = text.toLowerCase();
       setText(newText);
+      props.showAlert('coverted to lowercase','success');
     }
     const handleOnChange = (event)=>{
         setText(event.target.value);
@@ -17,36 +23,67 @@ export default function TextForm(props) {
         }else{
           setWordsCount(splitted_text.length);
         }
-       
+        //calculate number of sentance
+        setSentanceCount(getSentanceCount(text));
     }
     const handleClearClick = (event)=>{
       setText('');
       setWordsCount(0);
+      props.showAlert('Text Cleared','success');
     }
     const handleCopyClick = (event)=>{
       navigator.clipboard.writeText(text);
+      props.showAlert('Text Copied','success');
+    }
+    const handleExtraSpacesClick=(event)=>{
+      const filteredText = text.replace(/\s+/g,' ').trim();
+      setText(filteredText);
+      props.showAlert('Removed extra spaces','success');
+    }
+    const handleBeautify=(event)=>{
+        let newSentances = [];
+        const formattedText = text.replace(/\s+/g,' ').trim();
+        const sentances = formattedText.replace(/\.(?!\d)/g,'.|').split("|");
+        sentances.forEach(sentance => {
+          if(sentance[1]===''){
+            newSentances.push(sentance);
+          }else{
+            newSentances.push(' '+sentance);
+          }
+        });
+        let finalText='';
+        newSentances.forEach(newSentance => {
+          finalText=finalText+newSentance;
+        });
+        //incomplete
+        setText(finalText);
+        props.showAlert('Text beutfied','success');
+
     }
     const [text,setText] = useState('Enter Text here');
     const [wordsCount,setWordsCount] = useState(text.split(" ").length);
+    const [sentanceCount,setSentanceCount] = useState(text.replace(/\.(?!\d)/g,'.|').split("|").length);
 
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
           <h1>{props.heading}</h1>
           <div className="mb-2">
-              <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+              <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'gray':'white',color:props.mode==='dark'?'white':'black'}} value={text} id="myBox" rows="8"></textarea>
           </div>
           <button className="btn btn-primary mx-2" onClick = {handleUpClick}>Convert to Uppercase</button>
           <button className="btn btn-primary mx-2" onClick = {handleLowerClick}>Convert to Lowercase</button>
           <button className="btn btn-primary mx-2" onClick = {handleClearClick}>Clear Text</button>
           <button className="btn btn-primary mx-2" onClick = {handleCopyClick}>Copy To Clipboard</button>
+          <button className="btn btn-primary mx-2" onClick = {handleExtraSpacesClick}>Remove Extra Spaces</button>
+          <button className="btn btn-primary mx-2" onClick = {handleBeautify}>Beautiefy Sentance</button>
       </div>
-      <div className="container my-2">
+      <div className="container my-2" style={{color:props.mode==='dark'?'white':'black'}}>
           <h2>Your text summary</h2>
-          <p>{wordsCount} words,{text.length} characters</p>
+          <p>{wordsCount} words,{text.length} characters,{sentanceCount} sentances</p>
           <p>{0.008*wordsCount} Minutes Read</p>
           <h2>Preview</h2>
-          <p>{text}</p>
+          <p>{text.length > 0 ? text:"Enter somthing in the textbox to preview it here."}</p>
       </div>
     </>
   )
